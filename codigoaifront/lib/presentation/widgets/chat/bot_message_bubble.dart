87e01de6d1313b2f8d9084ce7ebb06e018ac74c0
Diego Lerma <dieglt_98@hotmail.com>
2024-04-23 03:06:41 -0600

    Adaptare pantalla de resumenes. Guardo previo a hacer cambios

--- codigoaifront/lib/presentation/widgets/chat/bot_message_bubble.dart
+++ codigoaifront/lib/presentation/widgets/chat/bot_message_bubble.dart
@@ -7,33 +7,69 @@ class BotMessageBubble extends StatelessWidget {
 
   const BotMessageBubble({super.key, required this.message});
 
+  Color _getMessageBackgroundColor(String messageText, ColorScheme colors) {
+    print("Processing message: $messageText"); // Añade esta línea para depurar
+    final lowerCaseText = messageText.toLowerCase();
+
+    if (lowerCaseText.contains('código rojo')) {
+      return Colors.red[100]!;
+    } else if (lowerCaseText.contains('código naranja')) {
+      return Colors.orange[100]!;
+    } else if (lowerCaseText.contains('código amarillo')) {
+      return Colors.yellow[100]!;
+    } else if (lowerCaseText.contains('código verde')) {
+      return Colors.green[100]!;
+    } else if (lowerCaseText.contains('código azul')) {
+      return Colors.blue[100]!;
+    } else if (lowerCaseText.contains('código naranja')) {
+      return Colors.orange[100]!; // Color naranja pastel
+    } else if (lowerCaseText.contains('código amarillo')) {
+      return Colors.yellow[100]!; // Color amarillo pastel
+    } else if (lowerCaseText.contains('código verde')) {
+      return Colors.green[100]!; // Color verde pastel
+    } else if (lowerCaseText.contains('código azul')) {
+      return Colors.blue[100]!; // Color azul pastel
+    }
+    // Color por defecto para mensajes normales
+    return colors.secondary;
+  }
+
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
+    final backgroundColor = _getMessageBackgroundColor(message.text, colors);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-            decoration: BoxDecoration(
-              color: colors.secondary,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              // Aquí usamos MarkdownBody para renderizar el mensaje
-              child: MarkdownBody(
-                data: message.text,
-                styleSheet:
-                    MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-                  p: const TextStyle(color: Colors.white), // Texto en markdown
-                  strong: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold), // Para el texto en negrita
+          width: double.infinity, // Ocupar toda la anchura disponible
+          decoration: BoxDecoration(
+            color: backgroundColor, // Color basado en el contenido del mensaje
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: MarkdownBody(
+              data: message.text,
+              styleSheet:
+                  MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                p: TextStyle(
+                  color: backgroundColor.computeLuminance() > 0.5
+                      ? Colors.black
+                      : Colors.white,
+                ),
+                strong: TextStyle(
+                  color: backgroundColor.computeLuminance() > 0.5
+                      ? Colors.black
+                      : Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )),
-        const SizedBox(
-            height: 10), // Mantenemos un pequeño espacio después del mensaje
+            ),
+          ),
+        ),
+        const SizedBox(height: 10), // Espacio después del mensaje
       ],
     );
   }
--- codigoaifront/lib/presentation/widgets/shared/message_field_box.dart
+++ codigoaifront/lib/presentation/widgets/shared/message_field_box.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class MessageFieldBox extends StatelessWidget {
+class MessageFieldBox extends StatefulWidget {
   final ValueChanged<String> onValue;
 
   const MessageFieldBox({super.key, required this.onValue});
 
   @override
-  Widget build(BuildContext context) {
-    final textController = TextEditingController();
-    final focusNode = FocusNode();
+  MessageFieldBoxState createState() => MessageFieldBoxState();
+}
 
+class MessageFieldBoxState extends State<MessageFieldBox> {
+  late TextEditingController textController;
+  late FocusNode focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+    focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    final text = textController.text.trim();
+    if (text.isNotEmpty) {
+      widget.onValue(text);
+      textController.clear();
+      // Mantener el foco en el campo de texto después de enviar el mensaje
+      FocusScope.of(context).requestFocus(focusNode);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -22,13 +51,15 @@ class MessageFieldBox extends StatelessWidget {
                 hintText: 'Escriba su mensaje...',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(color: Colors.transparent),
+                  borderSide: BorderSide.none,
                 ),
                 filled: true,
                 fillColor: Colors.white,
                 contentPadding: const EdgeInsets.symmetric(
                     horizontal: 20.0, vertical: 10.0),
               ),
+              textInputAction: TextInputAction.send,
+              onFieldSubmitted: (value) => _sendMessage(),
             ),
           ),
           const SizedBox(width: 8),
@@ -38,13 +69,7 @@ class MessageFieldBox extends StatelessWidget {
             child: IconButton(
               icon: const Icon(Icons.send),
               color: Colors.white,
-              onPressed: () {
-                if (textController.text.trim().isNotEmpty) {
-                  onValue(textController.text.trim());
-                  textController.clear();
-                  // focusNode.unfocus();
-                }
-              },
+              onPressed: _sendMessage,
             ),
           ),
         ],
